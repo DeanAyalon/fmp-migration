08cdff12334e399b7eed4623e3163a44b70aa3a8
Dean Ayalon (meshi) <dev@deanayalon.com>
2026-04-08 16:15:21 +0300

    stash

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-# Requires the FileMaker Data Migration Tool (https://community.claris.com/en/s/article/FileMaker-data-migration-tool)
+# Uses a FileMaker Server container
 
 # Context
 cd "$(dirname "$0")"
@@ -8,58 +8,49 @@ cd "$(dirname "$0")"
 
 # Utils
 help() {
-    if [ -z "$SOLUTION" ]; then 
-        echo "Use $0 <source path> <clone path> <migrated path>"
-        echo
-        echo "  e.g. $0 ./solution.fmp12 ./solution_dev.fmp12 ./solution_migrated.fmp"
-        echo '  Migrates from source solution.fmp12 using clone solution_dev.fmp12 into solution_migrated.fmp12'
-        echo
-        echo
-        echo Alternatively, you can set a 'SOLUTION' parameter within .env and run:
-        help_solution SOLUTION
-        echo
-        echo "To create .env, you can run:    echo SOLUTION=myapp > $(dirname "$0")/.env"
-    else 
-        printf "Use: "
-        help_solution $SOLUTION
-        echo
-        echo SOLUTION is set in .env to $SOLUTION
-    fi
+    echo "Use $0 <clone path> [migrated path]"
+    echo
+    echo "  e.g. $0 ./solution_dev.fmp12 ./solution_migrated.fmp"
+    echo '  Migrates from source database \$SOLUTION using clone solution_dev.fmp12 into solution_migrated.fmp12'
+    echo
     exit $1
 }
-help_solution() {
-    echo "$0 <source suffix> <clone suffix> [migrated suffix]"
-    echo
-    echo "  e.g. $0 prod dev"
-    echo "  Migrates from source ${1}_prod.fmp12 using clone ${1}_dev.fmp12 into ${1}_migrated.fmp12"
-}   
 fail() {
     echo $@
     exit 1
 }
 
 # Validate
-[ "$1" = "-h" ] && help 0
-[ -z "$2" ] && help 1
-[ -z "$SOLUTION" ] && [ -z "$3" ] && help 1
-migrated=${3:-migrated}
-# Check FMDataMigration
-FMDataMigration -version &> /dev/null || fail "Please install the FMDataMigrationTool: https://community.claris.com/en/s/article/FileMaker-data-migration-tool"
+[ "$1" = "-h" ] || [ "$1" = "help" ] && help 0
+[ -z "$1" ] && help 1
+migrated=${2:-migrated}
+[ -z "$FMS_CONTAINER" ] && FMS_CONTAINER=fms
+[ -z "$SOLUTION" ] && fail "Please set \$SOLUTION within .env"
+
+# source path
+[ -z "$DATABASES_PATH" ] && DATABASES_PATH="/opt/FileMaker/FileMaker Server/Data/Databases"
+SOURCE_PATH="$DATABASES_PATH/$SOLUTION.fmp12"
 
 # Credentials prompt
 read -p "Username: " USERNAME
 read -sp "Password: " PASSWORD
 
-# Migrate
-if [ -z "$SOLUTION" ]; then 
-    FMDataMigration -src_path "$1" -src_account $USERNAME -src_pwd "$PASSWORD" \
-        -clone_path "$2" -clone_account $USERNAME -clone_pwd "$PASSWORD" \
-        -target_path "$3" -ignore_valuelists -ignore_accounts -v > migration.log
-else
-    FMDataMigration -src_path "${SOLUTION}_$1.fmp12" -src_account $USERNAME -src_pwd "$PASSWORD" \
-        -clone_path "${SOLUTION}_$2.fmp12" -clone_account $USERNAME -clone_pwd "$PASSWORD" \
-        -target_path "${SOLUTION}_$migrated.fmp12" -ignore_valuelists -ignore_accounts -v > migration.log
-fi
+# Temporary folder inside container
+dir=/tmp/migration
+docker exec $FMS_CONTAINER mkdir -p $dir
+
+# Copy source and clone into container
+docker exec $FMS_CONTAINER cp "$SOURCE_PATH" $dir/source.fmp12
+docker cp "$1" $FMS_CONTAINER:$dir/clone.fmp12
+
+# Run migration inside container
+docker exec $FMS_CONTAINER FMDataMigration \
+    -src_path "$dir/source.fmp12" -src_account "$USERNAME" -src_pwd "$PASSWORD" \
+    -clone_path "$dir/clone.fmp12" -clone_account "$USERNAME" -clone_pwd "$PASSWORD" \
+    -target_path "$dir/$SOLUTION.fmp12" -ignore_valuelists -ignore_accounts -v | tee migration.log
+
+# Prompt to update live solution
+# TODO
 
 # Unset from memory to be safe in case the script is sourced
 unset PASSWORD
